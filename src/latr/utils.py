34c7beff8d9554fdd331a0dd77472462f72b45fa
uakarsh <55104596+uakarsh@users.noreply.github.com>
2022-06-19 19:36:44 +0530

    Added utility function for VQA

--- src/latr/utils.py
+++ src/latr/utils.py
@@ -1,5 +1,8 @@
 import random
 import torch
+import math
+from torch.nn.utils.rnn import pad_sequence
+
 
 def find_pad_idx(boxes):
   for i, j in enumerate(boxes):
@@ -42,3 +45,72 @@ def apply_mask_on_token_bbox(boxes, tokenized_words, only_actual_words = False, 
     boxes[temp, 5] = boxes[temp, 3] - boxes[temp, 1]
 
   return res,boxes, tokenized_words
+
+
+def convert_ans_to_token(answer, label2id, max_seq_length = 512 ):
+
+  ## Simple Trick to pad a sequence to deired length
+  dummy_array = torch.zeros(max_seq_length)
+  actual_ans_array = []
+
+  answer = answer.split(" ")
+  for token in answer:
+    actual_ans_array.append(label2id[token]['id'])
+  
+  actual_ans_array = torch.tensor(actual_ans_array, dtype = torch.int32)
+  actual_ans_array = pad_sequence([actual_ans_array,dummy_array], batch_first  = True)[0]
+
+  return actual_ans_array
+
+
+def convert_ques_to_token(question, tokenizer, pad_token_id = 0, max_seq_len = 512):
+
+  question_array = []
+  question = question.split(" ")
+  
+  for token in question:
+    question_array.extend(tokenizer(token, add_special_tokens = False).input_ids)
+  
+  if len(question_array)< max_seq_len:
+        question_array.extend([pad_token_id]* (max_seq_len-len(question_array)))
+
+  question_array = torch.tensor(question_array, dtype = torch.int32)
+  return question_array[:max_seq_len]
+
+
+## To be taken from here
+## https://logicatcore.github.io/scratchpad/lidar/sensor-fusion/jupyter/2021/04/20/3D-Oriented-Bounding-Box.html
+
+def rotate(origin, point, angle):
+    """
+    Rotate a point counterclockwise by a given angle around a given origin.
+    The angle should be given in radians.
+    
+    modified from answer here: https://stackoverflow.com/questions/34372480/rotate-point-about-another-point-in-degrees-python
+    """
+    # angle = np.deg2rad(angle)
+    ox, oy = origin
+    px, py = point
+
+    qx = ox + math.cos(angle) * (px - ox) - math.sin(angle) * (py - oy)
+    qy = oy + math.sin(angle) * (px - ox) + math.cos(angle) * (py - oy)
+    return int(qx), int(qy)
+
+
+def convert_token_to_ques(ques, tokenizer):
+  decoded_ques = tokenizer.decode(ques, skip_special_tokens=True)
+  return decoded_ques
+
+
+def convert_token_to_answer(ans, id2label):
+  non_zero_argument = torch.nonzero(ans,as_tuple = False).view(-1)
+
+  actual_answer = ans[non_zero_argument].cpu().numpy()
+  decoded_answer = []
+  
+  for token in actual_answer:
+    decoded_answer.append(id2label[token])
+  
+  decoded_answer = " ".join(decoded_answer)
+  return decoded_answer
+
